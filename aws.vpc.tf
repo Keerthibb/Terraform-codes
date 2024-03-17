a9295eae3a1259b1c12c4ae724303f822c68bab2
Keerthibb <92278245+Keerthibb@users.noreply.github.com>
2024-03-17 07:23:08 -0700

    Uploading the terraform files

---
+++ aws.vpc.tf
@@ -0,0 +1,79 @@
+resource "aws_vpc" "vpc" {
+  cidr_block       = "10.0.0.0/26"
+  instance_tenancy = "default"
+  enable_dns_support = true
+  enable_dns_hostnames =  true
+  tags = {
+    Name = "tfm-vpc"
+  }
+}
+
+resource "aws_subnet" "pub" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.0.0/28"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "IGW"
+  }
+}
+
+
+resource "aws_route_table" "rt1" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "custom"
+  }
+}
+
+resource "aws_route_table_association" "as_1" {
+  subnet_id      = aws_subnet.pub.id
+  route_table_id = aws_route_table.rt1.id
+}
+
+
+resource "aws_security_group" "sgp" {
+  name        = "fsg"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "My IP"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["192.168.56.1/32"]
+  }
+
+egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+tags = {
+  Name = "first-SG"
+}
+}
